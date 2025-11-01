afad22e086b1b2f3a70296ad23a345e00915edcb
n-s-iroagba <nnamdisolomon1@gmail.com>
2025-11-01 22:13:06 +0100

    a

--- frontend/src/components/forms/bank/BankForm.tsx
+++ frontend/src/components/forms/bank/BankForm.tsx
@@ -9,6 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { bankService } from '@/services';
+import { Bank } from '@/types/api';
 
 interface BankFormData {
   name: string;
